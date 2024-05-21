1aa33a0c43b0b8afc830a433c0d6ac2c24ba7e74
LeeHyungGeol <rjf1138@naver.com>
2024-05-21 21:44:49 +0900

    feat(security): allowPublicPathFilterChain

---
+++ src/main/java/sc/small/chat/auth/oauth2/entity/OAuthUserInfo.java
@@ -0,0 +1,1 @@
+package sc.small.chat.auth.oauth2.entity;public interface OAuthUserInfo {    String getProvider();    String getProviderId();    String getEmail();    String getName();    String getPicture();}
---
+++ src/main/java/sc/small/chat/auth/oauth2/entity/User.java
@@ -0,0 +1,1 @@
+package sc.small.chat.auth.oauth2.entity;public class User {}
