5663767d0d6583eb78ce0c8b209ed100298eec4d
LeeHyungGeol <rjf1138@naver.com>
2024-05-24 02:24:50 +0900

    feat(swagger): update prod domain

--- src/main/java/sc/small/chat/ChatApplication.java
+++ src/main/java/sc/small/chat/ChatApplication.java
@@ -1,10 +1,13 @@
 package sc.small.chat;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://api.smallchat.kr", description = "prod 도메인")})
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class ChatApplication {
 
